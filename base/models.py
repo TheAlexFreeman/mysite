a835668bf32b310858916dc0509895ce88a20080
Alex Freeman <alex@elyon.technology>
2026-02-13 20:31:00 -0800

    Initial base model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from __future__ import annotations
 
-# Create your models here.
+from django.contrib.postgres.indexes import BrinIndex
+from model_utils.fields import AutoCreatedField, AutoLastModifiedField
+from model_utils.models import TimeStampedModel
+
+
+class BaseModel(TimeStampedModel):
+    """
+    Abstract base model that includes common fields for all models.
+    """
+
+    created = AutoCreatedField("Created")
+    modified = AutoLastModifiedField("Modified")
+
+    class Meta:
+        abstract = True
+        indexes = [BrinIndex(fields=["created"])]
+        verbose_name = "Base Model"
+        verbose_name_plural = "Base Models"
