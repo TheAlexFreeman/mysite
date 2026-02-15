275edcb979fcfc8c75b2c20f8028e8869693b108
Alex Freeman <alex@elyon.technology>
2026-02-14 20:37:33 -0800

    Render URL in `ALLOWED_HOSTS`

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,7 +53,13 @@ if not IS_HEROKU_APP:
 if IS_HEROKU_APP:
     ALLOWED_HOSTS = ["*"]
 else:
-    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
+    ALLOWED_HOSTS = [
+        ".localhost",
+        "127.0.0.1",
+        "[::1]",
+        "0.0.0.0",
+        "thealexfreeman-ws.onrender.com",
+    ]
 
 
 # Application definition
